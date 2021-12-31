04e6ac7f23ea840196f84376e940a276a6f46d71
Sid Carroll <squidcarroll@gmail.com>
2021-12-30 23:24:53 -0700

    trying to configure firebase and google analytics

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
 const functions = require("firebase-functions");
+const { Nuxt } = require("nuxt-start");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const nuxtConfig = require("./nuxt.config.js");
+
+const config = {
+  ...nuxtConfig,
+  dev: false,
+  debug: false,
+  buildDir: ".nuxt",
+  publicPath: "public"
+};
+
+const nuxt = new Nuxt(config);
+
+let isReady = false;
+
+async function handleRequest(req, res) {
+  if (!isReady) {
+    try {
+      isReady = await nuxt.ready();
+    } catch (error) {
+      process.exit(1);
+    }
+  }
+  await nuxt.render(req, res);
+}
+
+exports.ssrapp = functions.https.onRequest(handleRequest);
---
+++ plugins/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCeJkOSJNKnYmLDSMQdYNpvSGygJwe8K4w",
+  authDomain: "kabloom-demo.firebaseapp.com",
+  projectId: "kabloom-demo",
+  storageBucket: "kabloom-demo.appspot.com",
+  messagingSenderId: "534888656244",
+  appId: "1:534888656244:web:35d6f5c428937a9f61d021",
+  measurementId: "G-KP9YLR55SM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = analytics.supported(getAnalytics(app));
